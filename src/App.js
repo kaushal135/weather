f7f3c7538b3c77553d239350ffc99304798975b8
Kaushal Joshi <kaushal_1995@yahoo.ca>
2020-09-23 19:49:15 -0400

    removed unnecessary code from index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <footer>
+        <div>
+          Icons from <a href="https://www.flaticon.com/" title="Freepik">Freepik</a> 
+        </div>
+      </footer>
     </div>
   );
 }
