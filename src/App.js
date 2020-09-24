2b9af5e497201e29eed44d68317908e9599a306b
Kaushal Joshi <kaushal_1995@yahoo.ca>
2020-09-24 16:02:29 -0400

    update app.js
    import use state, set up hooks, set up event functions, set up the JSX

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from './Assets/Weather_App_Logo.png';
 import './App.css';
 
+
 function App() {
+  let [units, setUnits] = useState(() => 'metric');
+  let [city, setCity] = useState(() => '');
+  let [result, setResult] = useState({});
+
+  const getData = (e) => {
+    if(e.key === 'Enter'){
+      console.log("hello world");
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-      <div>
-        <img src={Logo} alt=""/>
-      </div>
+        <div>
+          <img src={Logo} alt=""/>
+        </div>
+        <div>
+          <input 
+            type="text" 
+            placeholder="Enter a city..."
+            value={city}
+            onChange={(e) => setCity(e.target.value)}  
+            onKeyPress={getData}
+            />
+        </div>
+        <div>
+          <button onClick={() => setUnits('metric')}>C</button>  
+          <button onClick={() => setUnits('imperial')}>F</button>
+        </div>
       </header>
+
       <main>
 
       </main>
+      
       <footer className="Footer">
           Icons from <a href="https://www.flaticon.com/" title="Freepik">Freepik</a> 
       </footer>
